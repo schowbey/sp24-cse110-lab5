9f46de4d10b02e1ec0d25f2fcaa58ba77005eb2e
schowbey <103155665+schowbey@users.noreply.github.com>
2024-05-06 21:24:55 -0700

    writing tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
 Name: Shilpa Chowbey
+1) I would not use a unit test because there are a lot of moving features. The functionality depends on interactions across users (presumably across servers), so unit testing is only sufficient to capture the quality of individual features rather the app as a whole. That being said, it is still a useful test for the different steps in sending/receiving messages.
+
+2) I would use a unit test since this a specific case that checks the sending aspect of the application which is just a single feature.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,74 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// TESTING FUNCTIONS:: 
+// isPhoneNumber()
+test('correct format', () => {
+  expect(isPhoneNumber('(615) 309-7592')).toBe(true);
+});
+test('all the same numbers', () => {
+  expect(isPhoneNumber('(555) 555-5555')).toBe(true);
+});
+test('missing punctuation', () => {
+  expect(isPhoneNumber('6153097592')).toBe(false);
+});
+test('extra digits', () => {
+  expect(isPhoneNumber('61530997592')).toBe(false);
+});
+
+// isEmail()
+test('correct format', () => {
+  expect(isEmail('schowbey@ucsd.edu')).toBe(true);
+});
+test('only testing correct punctuation', () => {
+  expect(isEmail('hi@hi.hi')).toBe(true);
+});
+test('missing punctuation', () => {
+  expect(isEmail('schowbeyucsdedu')).toBe(false);
+});
+test('including random punctuation', () => {
+  expect(isEmail('s-chowbey@ucsd.edu')).toBe(false);
+});
+
+// isStrongPassword()
+test('strong password with only one character', () => {
+  expect(isStrongPassword('wwwwwwwww')).toBe(true);
+});
+test('strong password with punctuation and varying capitalization', () => {
+  expect(isStrongPassword('Hello_World')).toBe(true);
+});
+test('has special characters', () => {
+  expect(isStrongPassword('hello_world!!')).toBe(false);
+});
+test('weak password, too short', () => {
+  expect(isStrongPassword('hi')).toBe(false);
+});
+
+// isDate()
+test('correct MM-DD-YYYY format', () => {
+  expect(isDate('05/06/2024')).toBe(true);
+});
+test('correct MM-DD-YYYY format, technically invalid numbers', () => {
+  expect(isDate('13/40/0024')).toBe(true);
+});
+test('incorrect length of months days and years', () => {
+  expect(isDate('600/350/900')).toBe(false);
+});
+test('incorrect delimiter', () => {
+  expect(isDate('05-06-2024')).toBe(false);
+});
+
+// isHexColor()
+test('correct #xxxxxx, all numbers', () => {
+  expect(isHexColor('#123456')).toBe(true);
+});
+test('correct #xxxxxx, some letters', () => {
+  expect(isHexColor('#3f3f3f')).toBe(true);
+});
+test('invalid characters used', () => {
+  expect(isHexColor('#1j9o0l')).toBe(false);
+});
+test('wrong length', () => {
+  expect(isHexColor('451j9o0l')).toBe(false);
+});
